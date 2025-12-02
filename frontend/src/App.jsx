c7bac8aca600e778ac9748c866177cb7ab0a8f49
venetialiu <vl2289@nyu.edu>
2025-12-02 17:50:12 -0500

    setup App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { UserContext } from './context/UserContext'
+import './App.css'
+
 import About from './components/About/About'
 import Auth from './components/Auth/Auth'
 import Show from './components/Show/Show'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
@@ -20,11 +22,11 @@ function App() {
       <UserContext.Provider value={{ currentUser, setCurrentUser, isLoading }}>
         <Router>
           <Routes>
-            {/* auth is the first page users land on */}
+            {/* auth (login) is the first page users land on */}
             <Route path="/" element={<div className="App"><Auth /></div>} />
-            <Route path="/auth" element={<Meeting/>} />
-            <Route path="/about" element={<Home />} />
-            <Route path="/show" element={<Profile />} />
+            <Route path="/auth" element={<Auth/>} />
+            <Route path="/about" element={<About />} />
+            <Route path="/show" element={<Show />} />
             {/* to add: unique meeting page routes to ID */}
           </Routes>
         </Router>
