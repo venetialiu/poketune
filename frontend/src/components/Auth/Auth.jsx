e355e3d1ae4f16f08c2596df6e0f2512664104bc
venetialiu <vl2289@nyu.edu>
2025-12-06 14:12:03 -0500

    update Show.jsx

--- frontend/src/components/Auth/Auth.jsx
+++ frontend/src/components/Auth/Auth.jsx
@@ -7,14 +7,24 @@ import { UserContext } from '../../context/UserContext';
 const Auth = () => {
     const navigate = useNavigate();
     const { setCurrentUser } = useContext(UserContext);
+    const [hasError, setHasError] = useState(false);
 
-    
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const error = params.get('error');
+
+        if (error) {
+            setHasError(true);
+            console.log("Login error:", error);
+            alert("Login failed.");
+        } 
+    }, [])
+
     const handleClick = async () => {
         
         // redirects user to backend /auth/login route
         try {
             window.location.href = `${import.meta.env.VITE_BACKEND_URL}/api/auth/login`;
-            // navigate('/show');
 
         } catch(err){
             console.error("Login error: ", err);
@@ -24,7 +34,7 @@ const Auth = () => {
 
     return(
         <>
-            <h2>Hi! Please login to Spotify to find out what your pokemon is.</h2>
+            <h2>Hi! {hasError && "Login failed."} Please login{hasError && " again"} to Spotify{!hasError && " to find out what your pokemon is"}.</h2>
             <button onClick={handleClick}>Log In</button>
         </>
     )
--- frontend/src/components/Show/Show.jsx
+++ frontend/src/components/Show/Show.jsx
@@ -1,9 +1,42 @@
 import { useState, useEffect } from 'react';
 
 const Show = () => {
+    // shape of userdata TBD
+    const [userData, setUserData] = useState();
+
+    // fetch user data on mount
+    useEffect(() => {
+
+        const fetchUserData = async () => {
+            try {
+                const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/me`, {
+                    method: 'GET',
+                    // send cookies to backend
+                    credentials: "include", 
+                });
+                
+                const data = await res.json();
+
+                if (data.ok) {
+                    // shape of userdata TBD
+                    setUserData(data);
+                    console.log("User data successfully fetched.")
+                }
+
+            } catch (error) {
+                console.error('User data fetch error:', error);
+            } 
+        }
+
+        fetchUserData();
+        
+    }, [])
 
     return(
-        <h1>TBD</h1>
+        <div>
+            <h1>Your Pokemon</h1>
+            <pre>{JSON.stringify(userData, null, 2)}</pre>
+        </div>
         
     )
 
